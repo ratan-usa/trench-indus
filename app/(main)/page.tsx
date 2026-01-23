fb0c53dbdc699189b7f41270baded31cebf976c3
Ratan Prajapati <ratanprajapati1242@gmail.com>
2026-01-23 11:18:24 +0530

    3d video's added

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,11 +4,13 @@ import InfrastructureRisers from '../components/InfrastructureRisers'
 import PavingRisersCatalog from '../components/PavingRisersCatalog'
 import SolutionsSection from '../components/SolutionsSection'
 import IndustrySolutions from '../components/IndustrySolutions'
+import Product3DShowcase from '../../components/ui/sections/Product3DShowcase'
 
 const Home = () => {
   return (
     <div>
       <PavingRisersCatalog />
+      <Product3DShowcase />
       <SolutionsSection />
       <IndustrySolutions />
       <PipingRiserFeature />
